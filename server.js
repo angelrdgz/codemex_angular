90bdbe11f4e60926398e24c73c1ca77fa6f0330d
Ángel García <agarcia@grainchain.io>
2019-03-14 12:40:28 -0600

    again

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 //Install express server
+//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-// Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/codemex_angular'));
+app.use(express.static(__dirname + '/dist/codemex'));
 
-app.get('*', function(req,res) {
-  // Replace the '/dist/codemex_angular/index.html'
-  res.sendFile(path.join(__dirname + '/dist/codemex_angular/index.html'));
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname+'/dist/codemex/index.html'));
 });
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
